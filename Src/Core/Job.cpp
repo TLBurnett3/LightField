794597cca80aa1e2fa6281fd844a5077b2a30436
FoVI3D <tburnett@fovi3d.com>
2024-03-15 07:42:44 -0500

    Added Job

---
+++ Src/Core/Job.cpp
@@ -0,0 +1,368 @@
+//---------------------------------------------------------------------
+// MIT License
+// 
+// Copyright (c) 2024 TLBurnett3
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------
+
+// Thomas Burnett
+// Job.cpp
+//---------------------------------------------------------------------
+
+// Includes
+// System
+#include <iostream>
+#include <istream>
+#include <fstream>
+#include <sstream>
+
+// 3rdPartyLibs
+#include "glm/gtc/type_ptr.hpp"
+
+// Hogel Render
+#include "Job.h"
+
+using namespace Lf;
+using namespace Core;
+//---------------------------------------------------------------------
+
+
+#if (0)
+//---------------------------------------------------------------------
+// parseLights
+//---------------------------------------------------------------------
+int Job::parseLights(JSon::Value &doc)
+{
+int               rc = 0;
+JSon::Value&  v = doc["Lights"];
+
+  if (!v.is_null() && v.is_object())
+  {
+  JSon::Value::size_type n = v.size();
+
+    for (JSon::Value::size_type i = 0; i < n; i++)
+    {
+    std::string lite = "Light_" + std::to_string(i);
+    Light       lt;
+    glm::mat4   mL;
+    JSonConfig::parseLight(v, lite.c_str(), mL);
+
+      //uncomment line below to print string representation
+      //of lights from hrd file
+      //std::cout << v[0].dump() << std::endl;
+      lt.setProperties(mL);
+  
+      _lightLst.push_back(lt);
+    }
+  }
+  else
+  {
+    std::cout << "Error parsing Lights" << std::endl;
+    rc = -1;
+  }
+
+  return rc;
+}
+#endif
+
+
+//---------------------------------------------------------------------
+// printMatrix
+//---------------------------------------------------------------------
+void printMatrix(std::ostream &s,std::string idt,const char *str,const glm::mat4 &m)
+{
+  s << idt << str << std::endl;
+  s << idt << " " << m[0].x << " " << m[0].y << " " << m[0].z << " " << m[0].w << std::endl; 
+  s << idt << " " << m[1].x << " " << m[1].y << " " << m[1].z << " " << m[1].w << std::endl; 
+  s << idt << " " << m[2].x << " " << m[2].y << " " << m[2].z << " " << m[2].w << std::endl; 
+  s << idt << " " << m[3].x << " " << m[3].y << " " << m[3].z << " " << m[3].w << std::endl; 
+}
+
+
+//---------------------------------------------------------------------
+// parseModels
+//---------------------------------------------------------------------
+int Job::parseModels(JSon::Value &doc)
+{
+int              rc = 0;
+JSon::Value &v = doc["Models"];
+
+  if (!v.is_null() && v.is_array())
+  {
+		JSon::Value::size_type n = v.size();
+
+    for (JSon::Value::size_type i = 0;i < n;i++)
+    {
+    ModelDef          md;
+    int               r = 0;
+  	std::string       fName;
+
+			JSon::parse(v[i],"Transform",md._mT);
+
+      rc |= r = JSon::parse(v[i],"File",fName);
+
+      if (r == 0)
+      {
+      std::filesystem::path fPath = _dirPath;
+
+        fPath /= fName;
+          
+        if (std::filesystem::exists(fPath))
+        {
+          md._mPath = fPath.string();
+
+          _modelDefs.push_back(md);
+        }
+        else
+        {
+          std::cout << "Model_Path not found: " << fPath << std::endl;
+          rc |= -1;
+        }        
+      }
+    }
+  }
+  else 
+  {
+    std::cout << "Models is incorrect" << std::endl;
+    rc = -1;
+  }
+
+  return rc;
+}
+
+
+//---------------------------------------------------------------------
+// parseTasks
+//---------------------------------------------------------------------
+int Job::parseTasks(JSon::Value &doc)
+{
+int              rc = -1;
+JSon::Value &v = doc["Tasks"];
+
+  if (!v.is_null())
+  {
+		std::cout << std::endl;
+
+		JSon::parse(v, "ProofImage",         _tasks[ProofImage], true);
+		JSon::parse(v, "WriteAVI",           _tasks[WriteAVI], false);
+		JSon::parse(v, "WritePNG",           _tasks[WritePNG], false);
+		JSon::parse(v, "WriteDemo",          _tasks[WriteDemo], false);
+		JSon::parse(v, "WriteDepth",         _tasks[WriteDepth], false);
+		JSon::parse(v, "ProofDepth",         _tasks[ProofDepth], false);
+		JSon::parse(v, "HogelView",          _tasks[HogelView], false);
+		JSon::parse(v, "DisplayDepthBuffer", _tasks[DisplayDepthBuffer], false);
+
+    _tasks[FetchImageBuffer] = _tasks[ProofImage] | _tasks[WriteAVI] |_tasks[WritePNG] | _tasks[WriteDemo];
+    _tasks[FetchDepthBuffer] = _tasks[WriteDepth] | _tasks[ProofDepth];
+
+		std::cout << "FetchImageBuffer Task : " << (_tasks[FetchImageBuffer] ? "true" : "false") << std::endl;
+		std::cout << "FetchDeptchBuffer Task : " << (_tasks[FetchDepthBuffer] ? "true" : "false") << std::endl;
+		std::cout << "ProofImage Task : " << (_tasks[ProofImage] ? "true" : "false") << std::endl;
+		std::cout << "WriteAVI Task : " << (_tasks[WriteAVI] ? "true" : "false") << std::endl;
+		std::cout << "WritePNG Task : " << (_tasks[WritePNG] ? "true" : "false") << std::endl;
+		std::cout << "WriteDemo Task : " << (_tasks[WriteDemo] ? "true" : "false") << std::endl;
+		std::cout << "WriteDepth Task : " << (_tasks[WriteDepth] ? "true" : "false") << std::endl;
+		std::cout << "ProofDepth Task : " << (_tasks[ProofDepth] ? "true" : "false") << std::endl;
+		std::cout << "HogelView Task : " << (_tasks[HogelView] ? "true" : "false") << std::endl;
+		std::cout << "DisplayDepthBuffer Task : " << (_tasks[DisplayDepthBuffer] ? "true" : "false") << std::endl;
+
+		std::cout << std::endl;
+
+		rc = 0;
+  }
+
+  return rc;
+}
+
+
+//---------------------------------------------------------------------
+// parseDocument
+//---------------------------------------------------------------------
+int Job::parseDocument(JSon::Value &doc)
+{
+int rc = 0;
+
+	JSon::parse(doc, "JobName",    _jobName);
+	JSon::parse(doc, "Renderer",   _renderer);
+	JSon::parse(doc, "OutputPath", _outputPath);
+
+  if (_renderer == "Hogel")
+    _renderType = Hogel;
+  else if (_renderer == "Model")
+    _renderType = Model;
+  else if (_renderer == "Oblique")
+    _renderType = Oblique;
+  else if (_renderer == "Slice")
+    _renderType = Slice;
+  else if (_renderer == "MVP")
+    _renderType = MVP;
+  else
+    assert(0);
+
+  {
+		JSon::parse(doc, "FieldOfView",          _fov);
+		JSon::parse(doc, "HogelSize",            _hogelSize);
+    _hogelPitch = _hogelSize;
+		JSon::parse(doc, "HogelPitch",           _hogelPitch);
+		JSon::parse(doc, "NumHogels",            _numHogels);
+		JSon::parse(doc, "SliceMem",             _sliceMem);
+  }
+
+  rc |= JSon::parse(doc,"ViewVolume",_mVVT);
+  rc |= JSon::parse(doc,"Scene",_mScT);
+
+  rc |= parseModels(doc);
+//  rc |= parseLights(doc);
+  rc |= parseTasks(doc);
+ 
+  return rc;
+}
+
+
+//---------------------------------------------------------------------
+// parse
+//---------------------------------------------------------------------
+int Job::parse(const std::filesystem::path &fPath)
+{
+int rc    = -1;
+
+  if (std::filesystem::exists(fPath))
+  {
+	JSon::Value doc;
+
+		_filePath = fPath.string();
+		_dirPath = fPath.parent_path().string();
+
+		rc = JSon::read(_filePath,doc);
+
+		if (rc == 0)
+			rc = parseDocument(doc);
+  }
+
+  return rc;
+}
+
+
+//---------------------------------------------------------------------
+// print
+//---------------------------------------------------------------------
+void Job::print(std::ostream &o)
+{
+char  buf[128];
+
+  sprintf_s(buf,"%30s: %s","Job Name",_jobName.c_str());
+  o << buf << std::endl;
+
+  sprintf_s(buf,"%30s: %s","Directory Path",_dirPath.c_str());
+  o << buf << std::endl;
+
+  sprintf_s(buf,"%30s: %s","Renderer",_renderer.c_str());
+  o << buf << std::endl;
+
+  sprintf_s(buf,"%30s: %s","Output Path",_outputPath.c_str());
+  o << buf << std::endl;
+
+  sprintf_s(buf,"%30s: (%d,%d)","Num Hogels",_numHogels.x,_numHogels.y);
+  o << buf << std::endl;
+
+  sprintf_s(buf,"%30s: (%d,%d)","Hogel Size",_hogelSize.x,_hogelSize.y);
+  o << buf << std::endl;
+
+  sprintf_s(buf,"%30s: (%d,%d)","Hogel Pitch",_hogelPitch.x,_hogelPitch.y);
+  o << buf << std::endl;
+
+  sprintf_s(buf,"%30s: %d gig","Slice Memory",_sliceMem);
+  o << buf << std::endl;
+
+  {
+  size_t n = _modelDefs.size();
+  char buf2[32];
+
+    for (size_t i = 0;i < n;i++)
+    {
+      sprintf_s(buf2,"%s[%zd]","Model Path",i);
+      sprintf_s(buf,"%30s: %s",buf2,(char *)_modelDefs[i]._mPath.c_str());
+      o << buf << std::endl;
+    }
+  }
+
+#if (0)
+  {
+  size_t n = _lightLst.size();
+  char buf2[128];
+
+    for (size_t i = 0;i < n;i++)
+    {
+      sprintf_s(buf2,"Light %zd",i+1);
+      o << buf2 << std::endl;
+
+      sprintf_s(buf2,"  %15s: (%f,%f,%f)","Position",_lightLst[i].position().x,_lightLst[i].position().y,_lightLst[i].position().z);
+      o << buf2 << std::endl;
+
+      sprintf_s(buf2,"  %15s: (%f,%f,%f,%f)","Ambient",
+            _lightLst[i].ambient().r,_lightLst[i].ambient().g,_lightLst[i].ambient().b,_lightLst[i].ambient().w);
+      o << buf2 << std::endl;
+
+      sprintf_s(buf2,"  %15s: (%f,%f,%f,%f)","Diffuse",
+            _lightLst[i].diffuse().r,_lightLst[i].diffuse().g,_lightLst[i].diffuse().b,_lightLst[i].diffuse().w);
+      o << buf2 << std::endl;
+
+      sprintf_s(buf2,"  %15s: (%f,%f,%f,%f)","Specular",
+           _lightLst[i].specular().r,_lightLst[i].specular().g,_lightLst[i].specular().b,_lightLst[i].specular().w);
+      o << buf2 << std::endl;
+    }
+  }
+#endif
+}
+
+
+//---------------------------------------------------------------------
+// Job
+//---------------------------------------------------------------------
+Job::Job(void) : _filePath(),
+                 _dirPath(),
+                 _jobName(),
+                 _renderer("Model"),
+                 _renderType(Model),
+                 _modelDefs(),   
+ //                _lightLst(),
+                 _numHogels(0),
+                 _hogelSize(0),
+                 _hogelPitch(_hogelSize),
+                 _mVVT(1),
+                 _mScT(1),
+                 _fov(90),
+                 _sliceMem(0),
+                 _outputPath(),
+                 _tasks()
+{
+  for (int i = 0;i < TaskDefsNum;i++)
+    _tasks[i] = false;
+}
+
+
+//---------------------------------------------------------------------
+// ~Job
+//---------------------------------------------------------------------
+Job::~Job()
+{
+
+}
+
---
+++ Src/Core/Job.h
@@ -0,0 +1,211 @@
+//---------------------------------------------------------------------
+// MIT License
+// 
+// Copyright (c) 2024 TLBurnett3
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------
+
+// Thomas Burnett
+// Job.h
+
+#pragma once
+
+
+//---------------------------------------------------------------------
+// Includes
+// System
+#include <string>
+#include <vector>
+#include <filesystem>
+
+// 3rdPartyLibs
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+
+// LightField
+#include "Core/JSon.h"
+//#include "Light.h"
+
+//---------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------
+// Classes
+namespace Lf
+{
+  namespace Core
+  {
+    class ModelDef
+    {
+    // Defines
+      private:
+      protected:
+      public:
+
+        // Members
+      private:
+      protected:
+      public:
+        std::filesystem::path   _mPath;
+        glm::mat4               _mT;
+
+      // Methods
+      private:
+      protected:
+      public:
+        ModelDef(void) : _mPath(),
+                         _mT(1)
+        {}
+    };
+
+    class Job
+    {
+      // Defines
+      private:
+        enum RequiredDef
+        {
+          Required    = -1,
+          NotRequired = 0,
+        };
+      protected:
+        typedef std::vector<ModelDef>   ModelDefs;
+
+      public:
+        enum  TaskDefs
+        {
+          FetchImageBuffer = 0,        
+          FetchDepthBuffer,      
+          ProofImage,
+          WriteAVI,
+          WritePNG,
+          WriteDemo,
+          HogelView,
+          DisplayDepthBuffer,
+          WriteDepth,
+          ProofDepth,
+          TaskDefsNum,
+        };
+
+        enum RenderTypes
+        {
+          Unknown   = -1,
+          Model = 0,
+          Hogel,
+          Oblique,
+          Slice,
+          MVP,
+        };
+
+      // Members
+      private:
+      protected:
+        std::string						  _filePath;
+        std::string						  _dirPath;
+
+        std::string             _jobName;
+        std::string             _renderer;
+        int                     _renderType;
+
+        ModelDefs               _modelDefs;
+  //      LightLst                _lightLst;
+
+        glm::ivec2              _numHogels;
+        glm::ivec2              _hogelSize;             
+        glm::ivec2              _hogelPitch;             
+
+        glm::mat4               _mVVT;
+        glm::mat4               _mScT;
+
+        float                   _fov;     
+        uint32_t                _sliceMem;
+
+        std::string							_outputPath;
+
+        bool                    _tasks[TaskDefsNum];
+
+      public:   
+
+      // Methods
+      private:
+      protected:
+
+ //       int  parseLights    (JSon::Value &doc);
+        int  parseModels    (JSon::Value &doc);
+        int  parseTasks     (JSon::Value &doc);
+        int  parseDocument  (JSon::Value &doc);
+
+      public:
+
+        const std::string &jobName(void)
+        { return _jobName; }
+
+        const std::string &renderer(void)
+        { return _renderer; }
+
+        const int renderType(void)
+        { return _renderType; }
+
+ //       const LightLst &lightList(void)
+ //       { return _lightLst; }
+
+        const size_t numModels(void)
+        { return _modelDefs.size(); }
+
+        const std::filesystem::path &modelPath(const size_t i)
+        { return _modelDefs[i]._mPath; }
+
+        const glm::mat4 &modelTransform(const size_t i)
+        { return _modelDefs[i]._mT; }
+
+        const std::string &outputPath(void)
+        { return _outputPath; }
+
+        const glm::ivec2 &hogelSize(void)
+        { return _hogelSize; }
+
+        const glm::ivec2 &numHogels(void)
+        { return _numHogels; }
+
+        const uint16_t sliceMem(void)
+        { return _sliceMem; }
+
+        const glm::mat4 &viewVolumeTransform(void)
+        { return _mVVT; }
+
+        const glm::mat4 &sceneTransform(void)
+        { return _mScT; }
+
+        const float fov(void)
+        { return _fov; }
+
+        const bool isTask(TaskDefs t)
+        { return _tasks[t]; }
+
+        void print(std::ostream &o);
+
+        int parse(const std::filesystem::path &fPath);
+  
+        Job(void);
+        virtual ~Job();
+    };
+  };
+};
+ //---------------------------------------------------------------------
+
